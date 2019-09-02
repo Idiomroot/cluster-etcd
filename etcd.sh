226945feef5c6f35aa0083a3f3dbe38a707b764a
Idiom <34206700+Idiomroot@users.noreply.github.com>
2019-09-02 16:12:41 +0800

    Create etcd.sh

---
+++ etcd.sh
@@ -0,0 +1,62 @@
+
+cat > ca-config.json <<EOF
+{
+  "signing": {
+    "default": {
+      "expiry": "87600h"
+    },
+    "profiles": {
+      "www": {
+         "expiry": "87600h",
+         "usages": [
+            "signing",
+            "key encipherment",
+            "server auth",
+            "client auth"
+        ]
+      }
+    }
+  }
+}
+EOF
+ 
+# cat ca-csr.json
+cat > ca-csr.json <<EOF
+{
+    "CN": "etcd CA",
+    "key": {
+        "algo": "rsa",
+        "size": 2048
+    },
+    "names": [
+        {
+            "C": "CN",
+            "L": "Beijing",
+            "ST": "Beijing"
+        }
+    ]
+}
+EOF
+ 
+# cat server-csr.json
+cat > server-csr.json <<EOF
+{
+    "CN": "etcd",
+    "hosts": [
+    "172.16.3.83",
+    "172.16.3.90",
+    "172.16.3.197"
+    ],
+    "key": {
+        "algo": "rsa",
+        "size": 2048
+    },
+    "names": [
+        {
+            "C": "CN",
+            "L": "BeiJing",
+            "ST": "BeiJing"
+        }
+    ]
+}
+EOF
